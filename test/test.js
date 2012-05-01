bfb414807b589871ec66aecafa2f357f4e503adf
McLeopold <mcleopold@gmail.com>
2012-05-01 15:47:42 -0700

    add inline script ability to javascript tag

--- test/test.js
+++ test/test.js
@@ -1,13 +1,21 @@
 var Htmlr = require('../lib/htmlr.js').Htmlr
-  , assert = require('assert')
+  , assert = require('chai').assert
 ;
 
-describe('JavascriptHtmlr', function () {
-  describe('simple', function () {
-    it('div() should return <div />', function () {
+suite('JavascriptHtmlr', function () {
+  suite('simple', function () {
+    test('div() should return <div />', function () {
       with (Htmlr) {
         assert.deepEqual(div().render(), '<div />');
       }
     });
   });
+  suite('special', function () {
+		test('javascript function', function () {
+		  with (Htmlr) {
+		  	assert.deepEqual(javascript(function () {var h = "hello";}).render(),
+		  		               '<script>var h = "hello";</script>\n');
+		  }
+		})
+  })
 });
